62d8e9c848821f86fb7d1239b7960c69861e30e8
Kristoffer Kleine <kris.kleine@yahoo.de>
2013-07-13 10:26:54 +0200

    Switch to a new source tree structure using symlinks

--- build_tree.py
+++ build_tree.py
@@ -1,114 +1,95 @@
-import os, sys
+import os
+import sys
 
-from distutils.sysconfig import get_python_lib
 
-SITE_PACKAGES = get_python_lib()
-RUN_AGAIN = False
-IGNORE = ['bjoern']
+class InstallHandler(object):
 
-try:
-    import cream.dist
-except ImportError:
-    RUN_AGAIN = True
+    name = None
+    dirs = {}
+    files = {}
 
-class TreeError(Exception):
-    pass
+    def __init__(self, src_base_path, dest_base_path):
+        self.src_base_path = src_base_path
+        self.dest_base_path = dest_base_path
 
-class SetupData(object):
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
+    def get_files(self):
 
-    def __repr__(self):
-        return '<SetupData args=%r kwargs=%r>' % (self.args, self.kwargs)
+        f = []
+        for src_name in self.dirs.keys():
+            dest_name = self.dirs[src_name]
+            path = os.path.join(self.src_base_path, src_name)
+            for dirpath, dirnames, filenames in os.walk(path):
+                dirpath = os.path.relpath(dirpath, path)
+                for filename in filenames:
+                    src = os.path.join(self.src_base_path, src_name, dirpath, filename)
+                    dest = os.path.join(self.dest_base_path, dest_name, dirpath, filename)
+                    f.append((src, dest))
 
-def get_setup_data(filename):
-    with open(filename, 'r') as f:
-        setuppy = f.read()
-    # Replace setup() call
-    if not 'setup(' in setuppy:
-        raise TreeError('Malformed setup.py (no setup call): %s' % filename)
-    setuppy = setuppy.replace('setup(', 'setup_data = yay_setup(')
-    sandbox = {'yay_setup': SetupData}
-    cwd = os.getcwd()
-    os.chdir(os.path.abspath(os.path.dirname(filename)))
-    try:
-        exec setuppy in sandbox
-    except BaseException, e:
-        if (isinstance(e, ImportError) and str(e) == 'No module named cream.dist'):
-            # Ignore that error. (DIRTY!)
-            pass
-        else:
-            print 'Exception: %r' % e
-        return None
-    else:
-        setup_data = sandbox['setup_data']
-        if setup_data.args:
-            # A manifest! (O yeah, cream.dist exists)
-            setup_data.kwargs = cream.dist.get_pkg_info(*setup_data.args, **setup_data.kwargs)
-        return setup_data
-    finally:
-        os.chdir(cwd)
+        for src_name in self.files.keys():
+            dest_name = self.files[src_name]
+            src = os.path.join(self.src_base_path, src_name)
+            dest = os.path.join(self.dest_base_path, dest_name)
+            f.append((src, dest))
 
-def get_available_packages(root):
-    for dirpath, dirnames, filenames in os.walk(root):
-        if 'setup.py' in filenames:
-            yield dirpath
+        return f
 
-def get_package_locations(packages):
-    for packagedir in packages:
-        setup_data = get_setup_data(os.path.join(packagedir, 'setup.py'))
-        if setup_data:
-            if 'packages' in setup_data.kwargs:
-                # TODO: cream.dist
-                package_dir = setup_data.kwargs.get('package_dir', {})
-                for packagename in setup_data.kwargs['packages']:
-                    directory = os.path.join(packagedir, \
-                        package_dir.get(packagename, packagename.replace('.', os.path.sep)))
-                    yield (packagename, directory)
-            if 'py_modules' in setup_data.kwargs:
-                for modulename in setup_data.kwargs['py_modules']:
-                    yield (modulename, os.path.join(packagedir, modulename + '.py'))
 
-def build_tree(tree_root, locations):
-    tree_root = os.path.abspath(tree_root)
-    if not os.path.exists(tree_root):
-        os.makedirs(tree_root)
-    for name, fs_location in locations:
-        if name in IGNORE:
-            continue
-        fs_location = os.path.abspath(fs_location)
-        if os.path.isdir(fs_location):
-            # Package.
-            tree_location = os.path.join(tree_root, name.replace('.', os.path.sep))
-            print 'Installing %s to %s' % (name, tree_location)
-            if not os.path.exists(tree_location):
-                os.makedirs(tree_location)
-            for dirpath, dirnames, filenames in os.walk(fs_location):
-                abs_dirpath = os.path.normpath(os.path.join(fs_location, dirpath))
-                rel_dirpath = os.path.relpath(dirpath, fs_location)
-                for to_filename in filenames:
-                    create = os.path.join(tree_location, rel_dirpath, to_filename)
-                    if not os.path.islink(create):
-                        os.symlink(os.path.join(abs_dirpath, to_filename), create)
-                for dirname in dirnames:
-                    create = os.path.join(tree_location, rel_dirpath, dirname)
-                    if not os.path.exists(create):
-                        os.makedirs(create)
-        else:
-            # Module.
-            tree_location = os.path.join(tree_root, name + '.py')
-            print 'Installing %s to %s' % (name, tree_location)
-            if not os.path.islink(tree_location):
-                os.symlink(fs_location, tree_location)
+    def install(self):
+
+        for src, dest in self.get_files():
+            dest_dirname = os.path.dirname(dest)
+            if not os.path.exists(dest_dirname):
+                os.makedirs(dest_dirname)
+            print 'Installing {} to {}'.format(src, dest)
+            os.symlink(src, dest)
+
+
+class GpyconfInstallHandler(InstallHandler):
+    name = 'gpyconf'
+    dirs = {
+        'gpyconf/': 'gpyconf/'
+    }
+
+class PythonCreamInstallHandler(InstallHandler):
+    name = 'python-cream'
+    dirs = {
+        'cream/': 'cream/'
+    }
+
+class MelangeInstallHandler(InstallHandler):
+    name = 'melange'
+    dirs = {
+        'src/melange/': 'cream/melange/',
+        'data/': 'data/cream-melange'
+    }
+    files = {
+        'src/melange.py': 'melange.py'
+    }
+
+class MelangeWidgetsInstallHandler(InstallHandler):
+    name = 'melange-widgets'
+    dirs = {
+        'src/': 'data/cream-melange/widgets'
+    }
+
 
 if __name__ == '__main__':
-    print 'Usage: build_tree.py [ROOT] (defaults to site-packages)'
-    try:
-        root = sys.argv[1]
-    except IndexError:
-        root = SITE_PACKAGES
-    build_tree(root, list(get_package_locations(list(get_available_packages('.')))))
-    print '--- Done.'
-    if RUN_AGAIN:
-        print 'Please run build_tree.py again to install modules as well.'
+    if len(sys.argv) >= 3:
+        src_path = sys.argv[1]
+        target_path = sys.argv[2]
+        if not os.path.exists(target_path):
+            os.makedirs(target_path)
+        elif os.listdir(target_path):
+            print 'Install directory {} not empty. Exiting...'.format(target_path)
+            sys.exit(1)
+
+        print 'Installing to {}'.format(target_path)
+
+        for klass in InstallHandler.__subclasses__():
+            src = os.path.join(src_path, klass.name)
+            handler = klass(src, target_path)
+
+            print 'Installing {}'.format(klass.name)
+            handler.install()
+    else:
+        print 'Usage python build_tree.py src target'
